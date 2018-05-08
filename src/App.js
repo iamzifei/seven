0472c34492b25626da8c56ebf94b90b528161013
James Gong <james.gong@tiqk.com.au>
2018-05-08 21:05:46 +1000

    responsive

--- src/App.js
+++ src/App.js
@@ -5,47 +5,70 @@ import tile from './assets/tile.jpg';
 
 const Container = styled.section`
   background-color: #000;
-  padding: 96px;
   box-sizing: border-box;
   height: 100vh;
+  padding: 16px;
+  @media (min-width: 600px) {
+    padding: 48px;
+  }
+  @media (min-width: 960px) {
+    padding: 96px;
+  }
 `;
 
 const Section = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  width: ${props => props.width};
-  height: ${props => props.height};
-  background: url(${props => props.src});
+  position: relative;
 `;
 
 const Header = styled.section`
   display: flex;
-  align-items: center;
   background-color: rgba(0, 0, 0, 0.6);
+  align-items: center;
+  width: 100%;
+  max-width: 960px;
+  position: absolute;
+  bottom: 4px;
+  left: 0;
 `;
 
 const Logo = styled.div`
-  background: url(${props => props.src}) center;
-  background-size: contain;
-  width: ${props => props.width};
-  height: ${props => props.height};
   margin: 0 8px;
+  width: 10%;
 `;
 
 const Title = styled.h1`
-  font-size: 1.5em;
+  font-size: 1em;
   font-weight: bold;
   color: white;
+  @media (min-width: 600px) {
+    font-size: 1.2em;
+  }
+  @media (min-width: 960px) {
+    font-size: 1.5em;
+  }
+`;
+
+const ResponsiveImg = styled.img`
+  width: 100%;
+  height: auto;
+  max-width: 960px;
+`;
+
+const ResponsiveLogo = styled.img`
+  width: 100%;
+  height: auto;
 `;
 
 class App extends Component {
   render() {
     return (
       <Container>
-        <Section src={tile} width={'630px'} height={'354px'}>
+        <Section>
+          <ResponsiveImg src={tile} />
           <Header>
-            <Logo src={logo} width={'70px'} height={'70px'} />
+            <Logo>
+              <ResponsiveLogo src={logo} />
+            </Logo>
             <Title>Home and Away</Title>
           </Header>
         </Section>
